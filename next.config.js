b36f09c57ce6a48335f0477d4b8347a8193ad114
Larry Motuzis <mlauris2006@yahoo.com>
2024-03-15 11:30:52 -0500

    update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export', 
-  assetPrefix: '/portfolio/',
-  basePath: '/portfolio',
+  assetPrefix: '/potfolio/',
+  basePath: '/potfolio',
   trailingSlash: true,
 }
 
